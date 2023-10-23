8dbba8000055f0916b7ddc9638c1e80d7e88d712
nadiaMahfoud <nadiamahfoud48@gmail.com>
2023-10-23 09:12:01 -0700

    review

--- models/review.py
+++ models/review.py
@@ -1,10 +1,22 @@
-#!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+i#!/usr/bin/python3
+"""Review module for the HBNB project"""
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+""" Import necessary modules and classes """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+
+
+class Review(BaseModel, Base):
+    """Review class to store review information"""
+
+
+    """ Define the name of the SQL table"""
+    __tablename__ = 'reviews'
+
+
+    """ Define columns in the SQL table for review attributes"""
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    text = Column(String(1024), nullable=False)
+
