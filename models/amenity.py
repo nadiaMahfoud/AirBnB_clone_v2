fa97b455d27c374ec1d161a8ee99d839f8f44cc1
AmalNadifi <amal.nadifi9@gmail.com>
2023-10-23 09:42:06 -0700

    Updating the amenity file

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,30 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" This module is the 'Amenity' Module for HBNB project """
+from sqlalchemy import Column, String, Table, ForeignKey
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+import models
+from os import getenv
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """
+    This is the definition of the class for Amenity
+
+    Attributes:
+        name: name of the amenity
+    """
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "amenities"
+        name = Column(String(128), nullable=False)
+        # Relationship with Place for database storage (Many-to-Many)
+        place_amenities = relationship("Place", secondary=place_amenity,
+                                       viewonly=False,
+                                       back_populates="amenities")
+    else:
+        # Defining the name attribute for non-database storage
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """ This method Initializes Amenity """
+        super().__init__(*args, **kwargs)
