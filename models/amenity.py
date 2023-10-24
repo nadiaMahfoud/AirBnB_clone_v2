ffd5d268f9b48c1d5f79795f1942d358525f11f3
AmalNadifi <amal.nadifi9@gmail.com>
2023-10-24 10:49:12 -0700

    Fixed the code of amenity file

--- models/amenity.py
+++ models/amenity.py
@@ -15,12 +15,9 @@ class Amenity(BaseModel, Base):
     Attributes:
         name: name of the amenity
     """
-    if models.storage_t == 'db':
-        __tablename__ = 'amenities'
+    __tablename__ = 'amenities'
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
     else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
-        super().__init__(*args, **kwargs)
+        name = ' '
