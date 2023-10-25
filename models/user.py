5ee09df0f9d0bfbabfa86a81372bbe10b6bf5380
AmalNadifi <amal.nadifi9@gmail.com>
2023-10-25 13:42:02 -0700

    update reverse

--- models/user.py
+++ models/user.py
@@ -8,7 +8,6 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import hashlib
 
 
 class User(BaseModel, Base):
@@ -30,12 +29,3 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
-
-    @property
-    def password(self):
-        return self._password
-
-    @password.setter
-    def password(self, pwd):
-        """hashing password values"""
-        self._password = pwd
