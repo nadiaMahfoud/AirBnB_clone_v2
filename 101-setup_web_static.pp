630160f1897048733162a7a7dc9ef6044b661b7c
nadiaMahfoud <nadiamahfoud@gmail.com>
2023-11-12 13:56:52 -0800

    update

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -7,82 +7,86 @@ $nginx_conf = "server {
     add_header X-Served-By ${hostname};
     root   /var/www/html;
     index  index.html index.htm;
+
     location /hbnb_static {
         alias /data/web_static/current;
         index index.html index.htm;
     }
+
     location /redirect_me {
         return 301 https://github.com/nadiaMahfoud;
     }
+
     error_page 404 /404.html;
+
     location /404 {
-      root /var/www/html;
-      internal;
+        root /var/www/html;
+        internal;
     }
 }"
 
 package { 'nginx':
   ensure   => 'present',
-  provider => 'apt'
+  provider => 'apt',
 } ->
 
 file { '/data':
-  ensure  => 'directory'
+  ensure  => 'directory',
 } ->
 
 file { '/data/web_static':
-  ensure => 'directory'
+  ensure => 'directory',
 } ->
 
 file { '/data/web_static/releases':
-  ensure => 'directory'
+  ensure => 'directory',
 } ->
 
 file { '/data/web_static/releases/test':
-  ensure => 'directory'
+  ensure => 'directory',
 } ->
 
 file { '/data/web_static/shared':
-  ensure => 'directory'
+  ensure => 'directory',
 } ->
 
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => "Holberton School Puppet\n"
+  content => "Holberton School Puppet\n",
 } ->
 
 file { '/data/web_static/current':
   ensure => 'link',
-  target => '/data/web_static/releases/test'
+  target => '/data/web_static/releases/test',
 } ->
 
 exec { 'chown -R ubuntu:ubuntu /data/':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
+  path => '/usr/bin/:/usr/local/bin/:/bin/',
 }
 
 file { '/var/www':
-  ensure => 'directory'
+  ensure => 'directory',
 } ->
 
 file { '/var/www/html':
-  ensure => 'directory'
+  ensure => 'directory',
 } ->
 
 file { '/var/www/html/index.html':
   ensure  => 'present',
-  content => "Holberton School Nginx\n"
+  content => "Holberton School Nginx\n",
 } ->
 
 file { '/var/www/html/404.html':
   ensure  => 'present',
-  content => "Ceci n'est pas une page\n"
+  content => "Ceci n'est pas une page\n",
 } ->
 
 file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
-  content => $nginx_conf
+  content => $nginx_conf,
 } ->
 
-exec { 'nginx restart':
-  path => '/etc/init.d/'
+exec { 'nginx_restart':
+  command => '/etc/init.d/nginx restart',
 }
