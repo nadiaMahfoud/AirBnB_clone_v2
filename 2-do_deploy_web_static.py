beba17be74985a65a56704dd4310ef5cf64ecb5e
nadiaMahfoud <nadiamahfoud@gmail.com>
2023-11-12 14:17:07 -0800

    fixing

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,24 +4,31 @@
 from fabric.api import put, run, env
 from os.path import exists
 env.hosts = ['35.153.66.9', '54.237.124.135']
+env.user = 'ubuntu'
+env.identity = '~/.ssh/school'
+env.password = None
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-     if exists(archive_path) is False:
+    """
+    Deploys an archive to a server
+    """
+    if exists(archive_path) is False:
         return False
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
+        file_N = archive_path.split("/")[-1]
+        n = file_N.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('mkdir -p {}{}/'.format(path, n))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_N, path, n))
+        run('rm /tmp/{}'.format(file_N))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, n))
+        run('rm -rf {}{}/web_static'.format(path, n))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, n))
+        run('chmod -R 755 /data/')
+        print("New version deployed!")
         return True
-    except:
+    except FileNotFoundError:
         return False
