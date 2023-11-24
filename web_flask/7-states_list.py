6119cd8bc4ee57accb662e252d0d587b634cf445
nadiaMahfoud <nadiamahfoud48@gmail.com>
2023-11-24 10:58:05 -0800

    update

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" A script that Starts a Flask web application """
+from ../models import storage
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states():
+    """ it Returns an HTML page of all States sorted by name """
+    states = storage.all("State")
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """ it Removes the current SQLAlchemy session. """
+    storage.close()
+
+
+if __name__ == "__main__":
+    """ it Run on 0.0.0.0 """
+    app.run(host='0.0.0.0')
